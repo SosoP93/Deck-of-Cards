2ebb98e7bf043ea521988714db9be9c75e065149
SosoP93 <34781353+SosoP93@users.noreply.github.com>
2019-03-08 13:30:23 -0800

    Add files via upload

---
+++ deck_of_cards.py
@@ -0,0 +1,58 @@
+from random import shuffle
+
+class Card:
+
+	def __init__(self, suit, value):
+		self.suit = suit
+		self.value = value
+
+	def __repr__(self):
+		return f"{self.value} of {self.suit}"
+
+class Deck:
+
+	def __init__(self):
+		suits = ["Hearts", "Diamonds",  "Clubs", "Spades"]
+		values = list(("A,2,3,4,5,6,7,8,9,10,J,Q,K".split(",")))
+		self.cards = [Card(suit, value) for suit in suits for value in values]
+
+	def __repr__(self):
+		return f"Deck of {self.count()} cards"
+
+	def _deal(self, num):
+		removed = []
+		if self.count() == 0:
+			raise ValueError("All cards have been dealt")
+		else : 
+			while num:
+				num -= 1
+				removed.append(self.cards.pop(0))
+			return removed
+			 	
+	def count(self):
+		return len(self.cards)
+
+	def shuffle(self):
+		if self.count() != 52:
+			raise ValueError("Only full decks can be shuffled")	
+		return shuffle(self.cards)
+
+	def deal_card(self):
+		card = self._deal(1)
+		return card[0]
+
+	def deal_hand(self, hand_size):
+		hand = self._deal(hand_size)
+		return hand
+
+
+# first_deck = Deck()
+# print(first_deck)
+# first_deck.shuffle()
+# # print(first_deck.deal_card())
+# for i in first_deck.deal_hand(21): 
+# 	print(i)
+# print(first_deck)
+
+
+
